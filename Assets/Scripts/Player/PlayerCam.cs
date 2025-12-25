476e445e4f5239d5fb85e886033331cfea468d72
hjzre <142443853+hjzres@users.noreply.github.com>
2025-12-25 17:12:50 -0500

    Moved scripts and trying to make playercam work with the new inpus system

---
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerCam : MonoBehaviour
+    {
+        [SerializeField] Camera cam;
+        private float sens;
+
+        private float xRotation;
+        private float yRotation;
+        [SerializeField] InputActionReference look;
+        void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        void Update()
+        {
+            // float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens;
+            // float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens;
+
+            // yRotation += mouseX;
+            // xRotation -= mouseY;
+            // xRotation = Math.Clamp(xRotation, -90f, 90f);
+
+            // transform.rotation = Quaternion.Euler(0, yRotation, 0);
+
+            print(look.action.ReadValue<Vector2>());
+        }
+    }
+}
