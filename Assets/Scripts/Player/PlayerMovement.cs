d221460c0be60b63f0fe3d2f58b4a6801f90a64d
hjzre <142443853+hjzres@users.noreply.github.com>
2025-12-26 05:09:20 -0500

    Its sometimes not moving, will resolve soon

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -5,15 +6,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    // Components
     private Rigidbody _rb;
 
+    // Properties
+    [SerializeField] float moveSpeed;
+    private Vector3 _moveDirection;
+    [SerializeField] float groundDrag;
+
+    // Input
     private PlayerInput _playerInput;
     private InputAction _inputAction;
-    private Vector3 _moveDirection;
+
+    // Ground Check
+    private bool _isGrounded;
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask whatIsGround;
 
     void Awake()
     {
+
         _rb = GetComponent<Rigidbody>();
         _rb.freezeRotation = true;
 
@@ -29,7 +41,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        _moveDirection = new Vector3(_inputAction.ReadValue<Vector2>().x, 0, _inputAction.ReadValue<Vector2>().y);
+        _moveDirection = transform.forward * _inputAction.ReadValue<Vector2>().x + transform.right * _inputAction.ReadValue<Vector2>().y;
+
+        print(_moveDirection);
+
+        // _isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        // if (_isGrounded)
+        //     _rb.linearDamping = groundDrag;
+        // else
+        //     _rb.linearDamping = 0;
     }
 
     void FixedUpdate()
