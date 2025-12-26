90da30850163957cbd0a75b5b9f0826931f043c7
hjzre <142443853+hjzres@users.noreply.github.com>
2025-12-26 02:28:19 -0500

    Camera movement working

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -6,7 +6,7 @@ namespace Player
     public class PlayerCam : MonoBehaviour
     {
         [SerializeField] Camera cam;
-        private float sens;
+        [SerializeField] private float sens;
 
         private float xRotation;
         private float yRotation;
@@ -18,7 +18,6 @@ namespace Player
             Cursor.visible = false;
 
             look = lookaction.action;
-            look.Enable();
         }
 
         void OnEnable()
@@ -33,18 +32,16 @@ namespace Player
 
         void Update()
         {
-            // float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens;
-            // float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens;
-
-            // yRotation += mouseX;
-            // xRotation -= mouseY;
-            // xRotation = Math.Clamp(xRotation, -90f, 90f);
-
-            // transform.rotation = Quaternion.Euler(0, yRotation, 0);
-
             Vector2 looking = look.ReadValue<Vector2>();
-            if(looking != Vector2.zero)
-            Debug.Log(looking);
+
+            float mouseX = looking.x * Time.deltaTime * sens;
+            float mouseY = looking.y * Time.deltaTime * sens;
+
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         }
     }
 }
