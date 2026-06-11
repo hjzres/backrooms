088b35525d1f1c71a1c2b870a18987a533999030
Seb18456 <109159484+Nightbreaker18@users.noreply.github.com>
2026-06-11 16:27:42 -0400

    Transferred Logic to Job
    Uncleaned.

--- Assets/Scripts/Levels/Lobby.cs
+++ Assets/Scripts/Levels/Lobby.cs
@@ -89,12 +89,12 @@ namespace Assets.Scripts.Levels
 
             public Direction nextDir;
 
-            public readonly Direction[] directions;
+            public NativeArray<Direction> directions;
 
             public Point(float3 position, int maxDirections, ref Unity.Mathematics.Random prng)
             {
                 this.position = position;
-                directions = new Direction[maxDirections + 2];
+                directions = new(maxDirections + 2, Allocator.TempJob);
                 nextDir = Direction.NONE;
 
                 Direction lastDir = Direction.NONE;
@@ -127,6 +127,98 @@ namespace Assets.Scripts.Levels
             }
         }
 
+        private struct SegmentBuilder : IJob
+        {
+            public NativeArray<float3> vertices;
+
+            public NativeArray<int> triangles;
+
+            public float thickness;
+
+            public Point point;
+
+            public int maxWalls;
+
+            public void Execute()
+            {
+                CreateOriginVerts(point, ref vertices);
+
+                triangles[0] = 0;
+                triangles[1] = 1;
+                triangles[2] = 3;
+                triangles[3] = 1;
+                triangles[4] = 2;
+                triangles[5] = 3;
+
+                int verts = 4;
+                int tris = 6;
+
+                for (int i = 0; i <= maxWalls; i++)
+                {
+                    int distance = 5;
+
+                    Direction lastDir = point.directions[i];
+                    Direction nextDir = point.directions[i + 1];
+
+                    point.position += NormalizedDirections[(int)lastDir] * distance;
+
+                    float[] offsets = VertexOffsetTable(lastDir, nextDir, thickness);
+
+                    vertices[verts] = new float3(point.position.x + offsets[0], 0, point.position.z + offsets[1]);
+                    vertices[verts + 1] = new float3(point.position.x + offsets[0], 5, point.position.z + offsets[1]);
+                    vertices[verts + 2] = new float3(point.position.x + offsets[2], 5, point.position.z + offsets[3]);
+                    vertices[verts + 3] = new float3(point.position.x + offsets[2], 0, point.position.z + offsets[3]);
+
+                    triangles[tris] = verts;
+                    triangles[tris + 1] = verts + 1;
+                    triangles[tris + 2] = verts - 4;
+
+                    triangles[tris + 3] = verts + 1;
+                    triangles[tris + 4] = verts - 3;
+                    triangles[tris + 5] = verts - 4;
+
+                    triangles[tris + 6] = verts - 1;
+                    triangles[tris + 7] = verts - 2;
+                    triangles[tris + 8] = verts + 2;
+
+                    triangles[tris + 9] = verts - 1;
+                    triangles[tris + 10] = verts + 2;
+                    triangles[tris + 11] = verts + 3;
+
+                    verts += 4;
+                    tris += 12;
+                }
+
+                triangles[triangles.Length - 6] = verts - 2;
+                triangles[triangles.Length - 5] = verts - 3;
+                triangles[triangles.Length - 4] = verts - 1;
+                triangles[triangles.Length - 3] = verts - 4;
+                triangles[triangles.Length - 2] = verts - 1;
+                triangles[triangles.Length - 1] = verts - 3;
+            }
+
+            private readonly void CreateOriginVerts(Point point, ref NativeArray<float3> vertices)
+            {
+                Direction direction = point.directions[0];
+                var (x, z) = (point.position.x, point.position.z);
+
+                var offsets = direction switch
+                {
+                    Direction.UP => new[] { (x - thickness, z - thickness), (x - thickness, z - thickness), (x + thickness, z - thickness), (x + thickness, z - thickness) },
+                    Direction.RIGHT => new[] { (x - thickness, z + thickness), (x - thickness, z + thickness), (x - thickness, z - thickness), (x - thickness, z - thickness) },
+                    Direction.DOWN => new[] { (x + thickness, z + thickness), (x + thickness, z + thickness), (x - thickness, z + thickness), (x - thickness, z + thickness) },
+                    Direction.LEFT => new[] { (x + thickness, z - thickness), (x + thickness, z - thickness), (x + thickness, z + thickness), (x + thickness, z + thickness) },
+
+                    _ => throw new ArgumentException($"Direction not supported: {direction}")
+                };
+
+                vertices[0] = new Vector3(offsets[0].Item1, 0, offsets[0].Item2);
+                vertices[1] = new Vector3(offsets[1].Item1, 5, offsets[1].Item2);
+                vertices[2] = new Vector3(offsets[2].Item1, 5, offsets[2].Item2);
+                vertices[3] = new Vector3(offsets[3].Item1, 0, offsets[3].Item2);
+            }
+        }
+
         [Button]
         public void GenerateSimpleSegment() 
         {
@@ -137,49 +229,80 @@ namespace Assets.Scripts.Levels
 
             Point point = new(float3.zero, maxWalls, ref prng);
 
-            List <Vector3> vertices = new();
-            PlaceStarterVertices(point.directions[0], point, ref vertices);
+            Vector3[] vertices = new Vector3[point.directions.Length * 4];
+            int[] triangles = new int[(point.directions.Length * 12)];
 
-            List<int> triangles = new() { 0, 1, 3, 1, 2, 3 };
+            NativeArray<float3> vertsArr = new(vertices.Length, Allocator.TempJob);
+            NativeArray<int> trisArr = new(triangles.Length, Allocator.TempJob);
 
-            int verts = 4;
+            //List <Vector3> vertices = new();
+            //PlaceStarterVertices(point.directions[0], point, ref vertices);
 
-            for (int i = 0; i < maxWalls + 1; i++)
+            //List<int> triangles = new() { 0, 1, 3, 1, 2, 3 };
+
+            //int verts = 4;
+
+            //for (int i = 0; i < maxWalls + 1; i++)
+            //{
+            //    int distance = 5;
+
+            //    Direction lastDir = point.directions[i];
+            //    point.position += NormalizedDirections[(int)lastDir] * distance;
+
+            //    Direction nextDir = point.directions[i + 1];
+
+            //    float[] offsets = VertexOffsetTable(lastDir, nextDir, thickness);
+
+            //    vertices.AddRange(new List<Vector3>
+            //    {
+            //        new(point.position.x + offsets[0], 0, point.position.z + offsets[1]),
+            //        new(point.position.x + offsets[0], 5, point.position.z + offsets[1]),
+            //        new(point.position.x + offsets[2], 5, point.position.z + offsets[3]),
+            //        new(point.position.x + offsets[2], 0, point.position.z + offsets[3])
+            //    });
+
+            //    triangles.AddRange(new List<int>
+            //    {
+            //        verts, verts + 1, verts - 4,
+            //        verts + 1, verts - 3, verts - 4,
+            //        verts - 1, verts - 2, verts + 2,
+            //        verts - 1, verts + 2, verts + 3
+            //    });
+
+            //    verts += 4;
+            //}
+
+            //triangles.AddRange(new List<int> { verts - 2, verts - 3, verts - 1, verts - 4, verts - 1, verts - 3 });
+
+            SegmentBuilder job = new()
             {
-                int distance = 5;
+                vertices = vertsArr,
+                triangles = trisArr,
+                thickness = thickness,
+                point = point,
+                maxWalls = maxWalls
+            };
 
-                Direction lastDir = point.directions[i];
-                point.position += NormalizedDirections[(int)lastDir] * distance;
+            JobHandle handle = job.Schedule();
+            handle.Complete();
 
-                Direction nextDir = point.directions[i + 1];
-
-                float[] offsets = VertexOffsetTable(lastDir, nextDir, thickness);
-
-                vertices.AddRange(new List<Vector3>
-                {
-                    new(point.position.x + offsets[0], 0, point.position.z + offsets[1]),
-                    new(point.position.x + offsets[0], 5, point.position.z + offsets[1]),
-                    new(point.position.x + offsets[2], 5, point.position.z + offsets[3]),
-                    new(point.position.x + offsets[2], 0, point.position.z + offsets[3])
-                });
-
-                triangles.AddRange(new List<int>
-                {
-                    verts, verts + 1, verts - 4,
-                    verts + 1, verts - 3, verts - 4,
-                    verts - 1, verts - 2, verts + 2,
-                    verts - 1, verts + 2, verts + 3
-                });
-
-                verts += 4;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = vertsArr[i];
             }
 
-            triangles.AddRange(new List<int> { verts - 2, verts - 3, verts - 1, verts - 4, verts - 1, verts - 3 });
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                triangles[i] = trisArr[i];
+            }
+
+            vertsArr.Dispose();
+            trisArr.Dispose();
 
             Mesh mesh = new()
             {
-                vertices = vertices.ToArray(),
-                triangles = triangles.ToArray()
+                vertices = vertices,
+                triangles = triangles
             };
 
             mesh.RecalculateNormals();
@@ -187,37 +310,6 @@ namespace Assets.Scripts.Levels
             GameObject obj = new("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
             obj.GetComponent<MeshFilter>().mesh = mesh;
             obj.GetComponent<MeshRenderer>().material = gray;
-        }
-
-        private void PlaceStarterVertices(Direction direction, Point point, ref List<Vector3> vertices)
-        {
-            var (x, z) = (point.position.x, point.position.z);
-
-            var offsets = direction switch
-            {
-                Direction.UP => new[] { (x - thickness, z - thickness), (x - thickness, z - thickness), (x + thickness, z - thickness), (x + thickness, z - thickness) },
-                Direction.RIGHT => new[] { (x - thickness, z + thickness), (x - thickness, z + thickness), (x - thickness, z - thickness), (x - thickness, z - thickness) },
-                Direction.DOWN => new[] { (x + thickness, z + thickness), (x + thickness, z + thickness), (x - thickness, z + thickness), (x - thickness, z + thickness) },
-                Direction.LEFT => new[] { (x + thickness, z - thickness), (x + thickness, z - thickness), (x + thickness, z + thickness), (x + thickness, z + thickness) },
-
-                _ => throw new ArgumentException($"Direction not supported: {direction}")
-            };
-
-            vertices = new()
-            {
-                new Vector3(offsets[0].Item1, 0, offsets[0].Item2),
-                new Vector3(offsets[1].Item1, 5, offsets[1].Item2),
-                new Vector3(offsets[2].Item1, 5, offsets[2].Item2),
-                new Vector3(offsets[3].Item1, 0, offsets[3].Item2),
-            };
-        }
-
-        private struct SegmentBuilder : IJob
-        {
-            public void Execute()
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 }
